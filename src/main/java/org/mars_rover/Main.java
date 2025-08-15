e92b73e14d4be2018c5e31863ab1fbb95b1d9fb6
Timo Dahlhoff <sisasyco@gmail.com>
2025-08-15 23:21:39 +0200

    moved the code from the main function into a separate class

--- src/main/java/org/mars_rover/Main.java
+++ src/main/java/org/mars_rover/Main.java
@@ -1,37 +1,10 @@
 package org.mars_rover;
 
-import org.mars_rover.command.VehicleCommand;
-import org.mars_rover.grid_entity.vehicle.Rover;
-import org.mars_rover.printer.GridPrinter;
-import org.mars_rover.printer.VehiclePrinter;
-
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         try {
-            Scanner scanner = new Scanner(System.in);
-            InputHandler inputHandler = new InputHandler(scanner);
-            Plateau plateau = new Plateau();
-
-            Grid grid = plateau.getGrid();
-            GridPrinter gridPrinter = new GridPrinter(grid);
-
-            Rover rover = inputHandler.createRover();
-            VehiclePrinter vehiclePrinter = new VehiclePrinter(rover);
-            grid.placeGridEntity(rover);
-
-            gridPrinter.print();
-            vehiclePrinter.print();
-
-            List<VehicleCommand> commands = inputHandler.readCommands();
-            rover.executeCommands(commands);
-
-            gridPrinter.print();
-            vehiclePrinter.print();
-
-            scanner.close();
+            MarsExpedition marsExpedition = new MarsExpedition();
+            marsExpedition.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ src/main/java/org/mars_rover/MarsExpedition.java
@@ -0,0 +1,35 @@
+package org.mars_rover;
+
+import org.mars_rover.command.VehicleCommand;
+import org.mars_rover.grid_entity.vehicle.Rover;
+import org.mars_rover.printer.GridPrinter;
+import org.mars_rover.printer.VehiclePrinter;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class MarsExpedition {
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        InputHandler inputHandler = new InputHandler(scanner);
+        Plateau plateau = new Plateau();
+
+        Grid grid = plateau.getGrid();
+        GridPrinter gridPrinter = new GridPrinter(grid);
+
+        Rover rover = inputHandler.createRover();
+        VehiclePrinter vehiclePrinter = new VehiclePrinter(rover);
+        grid.placeGridEntity(rover);
+
+        gridPrinter.print();
+        vehiclePrinter.print();
+
+        List<VehicleCommand> commands = inputHandler.readCommands();
+        rover.executeCommands(commands);
+
+        gridPrinter.print();
+        vehiclePrinter.print();
+
+        scanner.close();
+    }
+}
